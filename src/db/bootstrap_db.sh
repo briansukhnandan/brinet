3c2b50794ca3afdb65ef8794cbf01cf5f7b0b2bf
Brian Sukhnandan <briansukhnandan@gmail.com>
2025-01-04 18:21:49 -0500

    small fix for bootstrap script

--- src/db/bootstrap_db.sh
+++ src/db/bootstrap_db.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sqlite3 brinet.db "VACUUM;"
-sqlite3 brinet.db < schema.sqlite
+echo "Bootstrapping the DB"
+sqlite3 ./src/db/brinet.db "VACUUM;"
+sqlite3 ./src/db/brinet.db < ./src/db/schema.sqlite
+echo "Finsihed bootstrapping the DB"
