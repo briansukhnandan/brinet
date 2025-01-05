bf5547d8e6133bf3be8756480901a905c4b3785b
Brian Sukhnandan <briansukhnandan@gmail.com>
2025-01-04 22:38:39 -0500

    change index

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { Logger } from './Logger';
 import { Context } from './Constants';
 
 const systemLogger = new Logger(Context.SYSTEM);
-async function main() {
+async function kickOffJobs() {
   if (!wasDbInitialized) {
     throw new Error(
       "DB was not initialized. Please run `db/bootstrap_db.sh`!"
@@ -14,11 +14,6 @@ async function main() {
   }
 
   systemLogger.log("Kicking off jobs...");
-  await kickOffJobs();
-}
-main();
-
-async function kickOffJobs() {
   await withDbc(async(dbc) => {
     systemLogger.log("Starting Congress Feed!");
     await maybeKickOffCongressFeed(dbc);
@@ -30,6 +25,6 @@ async function kickOffJobs() {
   });
 }
 
-const cronEveryMidnight = '0 0 * * *'; // Run once every day at midnight
-const MainJob = new CronJob(cronEveryMidnight, main); // change to scheduleExpressionMinute for testing
+const cronEveryMidnight = '0 0 * * *'; // Every day at midnight
+const MainJob = new CronJob(cronEveryMidnight, kickOffJobs);
 MainJob.start();
